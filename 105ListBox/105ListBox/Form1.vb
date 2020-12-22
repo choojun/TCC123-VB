343bcf7ef1f82f86a27ffe0e1053d99f6f1ee96e
Tan Choo Jun <cjtan@wou.edu.my>
2020-12-22 09:06:03 +0800

    Added 105ListBox

---
+++ 105ListBox/105ListBox/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(TextBox1.Text)
+        Label1.Text = TextBox1.Text + " is added into ListBox!"
+    End Sub
+End Class
