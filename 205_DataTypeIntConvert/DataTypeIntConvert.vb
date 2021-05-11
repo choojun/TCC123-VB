6a76ca7deecfba617892a5b1efe839dc1088ab0b
Tan Choo Jun <cjtan@wou.edu.my>
2021-05-11 22:41:07 +0800

    Added convert

---
+++ 205_DataTypeIntConvert/DataTypeIntConvert.vb
@@ -0,0 +1,14 @@
+﻿Module DataTypeIntConvert
+
+	Sub Main()
+		Dim number As String = ""
+		Console.WriteLine("Before conversion, the string representation of number Is " & number)
+		System.Console.Write("Enter a number:")
+		'Converts the specified String representation of 
+		'a number to an equivalent 64-bit signed integer
+		number = System.Convert.ToInt64(Console.ReadLine())
+		number = number + 1
+		Console.WriteLine("")
+		Console.WriteLine("After conversion, the integer value after calculation Is " & number)
+	End Sub
+End Module
