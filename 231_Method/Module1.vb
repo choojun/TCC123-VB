c4047a29630df1998ca41767b902ce86dda43342
Tan Choo Jun <cjtan@wou.edu.my>
2021-05-31 15:05:13 +0800

    Bugs fixed

--- 231_Method/Module1.vb
+++ 231_Method/Module1.vb
@@ -5,7 +5,7 @@
     End Function
 
     Sub printMe(ByVal a As String, ByVal b As Integer)
-        Console.WriteLine("Output: " & b)
+        Console.WriteLine("Output: " & a & b)
     End Sub
 
     Function multiplyMe(ByVal a As Integer, Optional ByVal b As Integer = 2) As Integer
@@ -17,12 +17,12 @@
         Dim number1 As Integer = 3
         Dim number2 As Integer = 6
         Dim total1 As Integer = sumMe(number1, number2)
-        printMe("total1: {0}", total1)
+        printMe("total1 = ", total1)
 
         ' Optional Argument
         Dim total3 As Integer = multiplyMe(7) ' a = 7, b = 2
-        printMe("total3: {0}", total3)
+        printMe("total3 = ", total3)
         Dim total4 As Integer = multiplyMe(7, 3) ' a = 7, b = 3
-        printMe("total4: {0}", total4)
+        printMe("total4 = ", total4)
     End Sub
 End Module
